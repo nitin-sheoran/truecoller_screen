b29fee228bd8d06f4cab7017d9965a6a6b48fe43
Nitin <nsheoran2005@gmail.com>
2023-09-09 21:22:56 +0530

    generalScreen add Divider

--- lib/general_screen.dart
+++ lib/general_screen.dart
@@ -99,6 +99,7 @@ class _GeneralScreenState extends State<GeneralScreen> {
                         ),
                       ],
                     ),
+                    Divider(),
                   ],
                 ),
               ),
